7c1e1ae64babcff4115e143d775186369a954d11
jovid1242 <jovid1242@gmail.com>
2022-04-28 17:35:30 +0500

    fixing

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,14 +1,5 @@
 import React from "react";
-import Home from "../../pages/Home";
-import Router from "../../router/Router";
-import XAbout from "../About/XAbout";
-import XHeader from "../Header/XHeader";
 
 export default function Main() {
-  return (
-    <>
-      <XHeader />
-      <Router />
-    </>
-  );
+  return <>main</>;
 }
