48cc417982fa96fafd254e9795d22df05f93cf49
thenavik <iamdigitalist@gmail.com>
2022-05-02 15:38:36 +0300

    fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FaWhatsapp, FaTelegram, FiGithub, FaLinkedinIn } from "react-icons/fa";
 import { AiOutlineGithub } from "react-icons/ai";
 import { BsMessenger } from "react-icons/bs";
+import { CgScrollV } from "react-icons/cg";
 import ".././styles/home/xhome.scss";
 import AvatarIcon from ".././styles/home/avatar-1.svg";
 import XAbout from "../components/About/XAbout";
@@ -9,14 +10,30 @@ import XSkills from "../components/Skills/XSkills";
 import XExperience from "../components/Experience/XExperience";
 import XWork from "../components/Works/XWorks";
 import XBlog from "../components/Blog/XBlog";
-import XFooter from "../components/Footer/XFooter";
+import XFooter from "../components/footer/XFooter";
+// import Particles from "react-particles-js";
 
 export default function Home() {
   return (
     <div>
-      <div className="container_ofolio">
-        <div className="home">
-          <img src={AvatarIcon} />
+      <div className="home">
+        {/* <section>
+          <Particles
+            params={{
+              particles: {
+                shape: {
+                  type: "images",
+                  image: [
+                    { src: "path/to/first/image.svg", height: 20, width: 20 },
+                    { src: "path/to/second/image.jpg", height: 20, width: 20 },
+                  ],
+                },
+              },
+            }}
+          />
+        </section> */}
+        <div className="container_ofolio">
+          <img className="avatar" src={AvatarIcon} />
           <h1 className="user-name">Bolby Doe</h1>
           <span>I'm a frontend developer</span>
           <ul className="social-icons">
@@ -47,6 +64,10 @@ export default function Home() {
             </li>
           </ul>
           <button className="button">Contact Me</button>
+          <div className="scroll">
+            <p>Scroll Down</p>
+            <CgScrollV />
+          </div>
         </div>
       </div>
       <XAbout />
